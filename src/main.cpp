065822ad79d3214439b2800fc312348943c450ee
krmiller678 <krmiller678@gmail.com>
2025-01-07 07:14:33 -0500

    Continued work on CMake this am. Set up build folders -> simple project so just running everything to release currently. ofstream started.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <exception>
 #include "Event.h"
 #include "ToDo.h"
 using namespace std;
@@ -9,11 +11,30 @@ This program is designed to serve as a weekly calendar to keep track of importan
 enable a to-do list to be constructed from stored memory, and keep track of long term goals.
 Inspired by Kyle's Office Chalkboard :D
 */
+
+void EventReadIn(vector<Event>& dailyEvents);
+void ToDoReadIn(vector<Event>& toDos);
+void EventWriteOut(vector<Event>& dailyEvents, string dayOfWeek);
+void ToDoWriteOut(vector<Event>& dailyEvents, string toDoCategory);
+
 int main () {
     vector<Event> mondayEvents;
     vector<ToDo> weeklyToDos;
-    mondayEvents.push_back(Event("11 am to ", "12 pm: ", "NapTime"));
+    mondayEvents.push_back(Event("11 am to ", "12 pm: ", "NapTime!"));
     cout << mondayEvents[0].getStartTime() << mondayEvents[0].getEndTime() << mondayEvents[0].getTitle();
-    
+    EventWriteOut(mondayEvents, "Monday");
     return 0;
+}
+
+void EventWriteOut(vector<Event>& dailyEvents, string dayOfWeek) {
+    string fullPath = "events/" + dayOfWeek + ".txt";
+    ofstream outputFile(fullPath);
+    if (outputFile.is_open()) {
+        outputFile << "testing";
+        outputFile.close();
+        cout << "testing";
+    }
+    else {
+        throw invalid_argument( "error opening file. \n");
+    }
 }
