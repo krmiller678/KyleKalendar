539df10d11d28bf190f6ebddc921bbfcb4c3b024
Kyle Miller <134568123+krmiller678@users.noreply.github.com>
2025-01-22 16:54:05 -0500

    Delete KyleKalendarQt/build directory

--- KyleKalendarQt/build/Desktop_Qt_6_8_1_MinGW_64_bit-Release/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_KyleKalendarQt_FILE Z:/KyleKalendar/KyleKalendarQt/build/Desktop_Qt_6_8_1_MinGW_64_bit-Release/KyleKalendarQt.exe)
-set(__QT_DEPLOY_TARGET_KyleKalendarQt_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_KyleKalendarQt_RUNTIME_DLLS C:/Qt/6.8.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Core.dll)
